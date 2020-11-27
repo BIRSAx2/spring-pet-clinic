9b757bf36529a4dddd6f78fd0c0a618daf84a9d3
BIRSAx2 <Mouhieddinesabir@gmail.com>
2020-11-27 17:29:57 +0100

    Adds PetTypes in Bootstrap (startup). Closes #32

--- pet-clinic-web/src/main/java/dev/mouhieddine/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/dev/mouhieddine/springpetclinic/bootstrap/DataLoader.java
@@ -1,55 +1,67 @@
 package dev.mouhieddine.springpetclinic.bootstrap;
 
 import dev.mouhieddine.springpetclinic.model.Owner;
+import dev.mouhieddine.springpetclinic.model.PetType;
 import dev.mouhieddine.springpetclinic.model.Vet;
 import dev.mouhieddine.springpetclinic.services.OwnerService;
+import dev.mouhieddine.springpetclinic.services.PetTypeService;
 import dev.mouhieddine.springpetclinic.services.VetService;
-import dev.mouhieddine.springpetclinic.services.map.OwnerServiceMap;
-import dev.mouhieddine.springpetclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DataLoader implements CommandLineRunner {
 
-    private final OwnerService ownerService;
-    private final VetService vetService;
+  private final OwnerService ownerService;
+  private final VetService vetService;
+  private final PetTypeService petTypeService;
 
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
-        this.ownerService = ownerService;
-        this.vetService = vetService;
-    }
+  public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+    this.ownerService = ownerService;
+    this.vetService = vetService;
+    this.petTypeService = petTypeService;
+  }
 
-    @Override
-    public void run(String... args) throws Exception {
+  @Override
+  public void run(String... args) throws Exception {
 
-        System.out.println("Loading owners...");
+    System.out.println("Loading pets...");
 
-        Owner owner1 = new Owner();
-        owner1.setFirstName("Mouhieddine");
-        owner1.setLastName("Sabir");
+    PetType cat = new PetType();
+    cat.setName("Tom");
+    PetType savedCatPetType = petTypeService.save(cat);
 
-        ownerService.save(owner1);
+    PetType dog = new PetType();
+    dog.setName("Jerry");
+    PetType savedDogPetType = petTypeService.save(dog);
 
-        Owner owner3 = new Owner();
-        owner3.setFirstName("Benicio");
-        owner3.setLastName("Del Toro");
+    System.out.println("Loading owners...");
 
-        ownerService.save(owner3);
+    Owner owner1 = new Owner();
+    owner1.setFirstName("Mouhieddine");
+    owner1.setLastName("Sabir");
 
-        System.out.println("Loading vets ...");
+    ownerService.save(owner1);
 
-        Vet vet1 = new Vet();
-        vet1.setFirstName("Brad");
-        vet1.setLastName("Pitt");
-        vetService.save(vet1);
+    Owner owner3 = new Owner();
+    owner3.setFirstName("Benicio");
+    owner3.setLastName("Del Toro");
 
-        Vet vet2 = new Vet();
-        vet2.setFirstName("Jennifer");
-        vet2.setLastName("Aniston");
-        vetService.save(vet2);
+    ownerService.save(owner3);
+
+    System.out.println("Loading vets...");
+
+    Vet vet1 = new Vet();
+    vet1.setFirstName("Brad");
+    vet1.setLastName("Pitt");
+    vetService.save(vet1);
+
+    Vet vet2 = new Vet();
+    vet2.setFirstName("Jennifer");
+    vet2.setLastName("Aniston");
+    vetService.save(vet2);
 
 
-    }
+  }
 }
