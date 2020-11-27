122b8846dd492ca643aedc26a0cca7067b666eb2
BIRSAx2 <Mouhieddinesabir@gmail.com>
2020-11-27 18:27:40 +0100

    Refactoring Specialty Map Service Impl. Closes #31

--- pet-clinic-data/src/main/java/dev/mouhieddine/springpetclinic/services/map/VetMapService.java
+++ pet-clinic-data/src/main/java/dev/mouhieddine/springpetclinic/services/map/VetMapService.java
@@ -1,6 +1,7 @@
 package dev.mouhieddine.springpetclinic.services.map;
 
 import dev.mouhieddine.springpetclinic.model.Vet;
+import dev.mouhieddine.springpetclinic.services.SpecialtyService;
 import dev.mouhieddine.springpetclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -9,28 +10,42 @@ import java.util.Set;
 @Service
 public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
 
-    @Override
-    public Set<Vet> findAll() {
-        return super.findAll();
-    }
+  private SpecialtyService specialtyService;
 
-    @Override
-    public Vet findById(Long id) {
-        return super.findById(id);
-    }
+  public VetMapService(SpecialtyService specialtyService) {
+    this.specialtyService = specialtyService;
+  }
 
-    @Override
-    public Vet save(Vet object) {
-        return super.save(object);
-    }
+  @Override
+  public Set<Vet> findAll() {
+    return super.findAll();
+  }
 
-    @Override
-    public void delete(Vet object) {
-        super.delete(object);
-    }
+  @Override
+  public Vet findById(Long id) {
+    return super.findById(id);
+  }
 
-    @Override
-    public void deleteById(Long id) {
-        super.deleteById(id);
+  @Override
+  public Vet save(Vet object) {
+    if (object == null) return null;
+
+    if (object.getSpecialties().size() > 0) {
+      object.getSpecialties().forEach(specialty -> {
+        if (specialty.getId() == null)
+          specialty.setId(specialtyService.save(specialty).getId()); // setting the specialty id to the saved specialty id
+      });
     }
+    return super.save(object);
+  }
+
+  @Override
+  public void delete(Vet object) {
+    super.delete(object);
+  }
+
+  @Override
+  public void deleteById(Long id) {
+    super.deleteById(id);
+  }
 }
