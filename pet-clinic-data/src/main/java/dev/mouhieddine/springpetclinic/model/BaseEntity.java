52553d4ca4662df574488611e532dea61834ce11
BIRSAx2 <Mouhieddinesabir@gmail.com>
2020-11-30 16:57:55 +0100

    Updated BaseEntity with JPA Annotations. Closes #35

--- pet-clinic-data/src/main/java/dev/mouhieddine/springpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/dev/mouhieddine/springpetclinic/model/BaseEntity.java
@@ -1,14 +1,23 @@
 package dev.mouhieddine.springpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY) // IDENTITY->AUTO INCREMENT in MySQL
+  private Long id;
 
-    public Long getId() {
-        return id;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public void setId(Long id) {this.id = id;}
+  public void setId(Long id) {
+    this.id = id;
+  }
 }
