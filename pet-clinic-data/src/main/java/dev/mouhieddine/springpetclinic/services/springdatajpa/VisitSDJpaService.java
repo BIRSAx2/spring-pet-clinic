0f7535814a04c3fcfe27153240c1f8f14ce7bd03
BIRSAx2 <Mouhieddinesabir@gmail.com>
2020-11-30 18:39:11 +0100

    Created a Visit Spring Data JPA Service. Closes #47

---
+++ pet-clinic-data/src/main/java/dev/mouhieddine/springpetclinic/services/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,48 @@
+package dev.mouhieddine.springpetclinic.services.springdatajpa;
+
+import dev.mouhieddine.springpetclinic.model.Visit;
+import dev.mouhieddine.springpetclinic.repositories.VisitRepository;
+import dev.mouhieddine.springpetclinic.services.VisitService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author : Mouhieddine.dev
+ * @created : 11/30/2020, Monday
+ **/
+public class VisitSDJpaService implements VisitService {
+
+  VisitRepository visitRepository;
+
+  public VisitSDJpaService(VisitRepository visitRepository) {
+    this.visitRepository = visitRepository;
+  }
+
+  @Override
+  public Set<Visit> findAll() {
+    Set<Visit> visits = new HashSet<>();
+    visitRepository.findAll().forEach(visits::add);
+    return visits;
+  }
+
+  @Override
+  public Visit findById(Long id) {
+    return visitRepository.findById(id).orElse(null);
+  }
+
+  @Override
+  public Visit save(Visit visit) {
+    return visitRepository.save(visit);
+  }
+
+  @Override
+  public void delete(Visit visit) {
+    visitRepository.delete(visit);
+  }
+
+  @Override
+  public void deleteById(Long id) {
+    visitRepository.deleteById(id);
+  }
+}
